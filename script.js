388c23af7329be750ef377f5a43a4712ac338de5
joelmessenger <160664332+joelmessenger@users.noreply.github.com>
2024-02-28 11:42:34 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const title = document.queryselector('.title')
+
 document.addeventlistener('scroll',function(){
   let value = windows.scrolly
   // console.log(value)
-  title.style.margintop = value *2 + 'px'
+  title.style.margintop = value * 2 + 'px'
 }
