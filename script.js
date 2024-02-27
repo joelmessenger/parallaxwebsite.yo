349fb895b8ae44eef7d034d9023b8b89b4f8aa09
joelmessenger <160664332+joelmessenger@users.noreply.github.com>
2024-02-28 12:12:00 +1300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("hello")
 const title = document.querySelector('.title')
 
 document.addEventListener('scroll',function() {
-  let value = windows.scrollY
+  let value = window.scrollY
   // console.log(value)
   
   title.style.marginTop = value * 2 + 'px'
