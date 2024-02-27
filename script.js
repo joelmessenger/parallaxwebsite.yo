2293850d1d94f02b43895876fda40df8f648c640
joelmessenger <160664332+joelmessenger@users.noreply.github.com>
2024-02-28 12:01:07 +1300

    d

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const title = document.querySelector('.title')
 
-document.addEventListener('scroll',function(){
+document.addEventListener('scroll',function() {
   let value = windows.scrollY
   // console.log(value)
   title.style.marginTop = value * 2 + 'px'
-)}
+})
